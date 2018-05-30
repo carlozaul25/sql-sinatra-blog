2d98ceb21ee288faa3978c61d142ccf9e7c71447
carlozaul25 <carloz18rivera@live.com>
2018-05-30 15:58:10 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_052952) do
+ActiveRecord::Schema.define(version: 2018_05_30_184334) do
+
+  create_table "user", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
